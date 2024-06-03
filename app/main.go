ba2c10b433a5f9a993cfdcb3dee79a74361e35da
jet2tlf <jet2tlf@gmail.com>
2024-06-03 03:29:54 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,19 +4,46 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"syscall"
 )
 
 func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+	tmpDir := "/tmp/mydocker"
 
 	cmd := exec.Command(command, args...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	output, err := cmd.Output()
+
+	_ = os.Mkdir(tmpDir, 0755)
+
+	err := exec.Command("mkdir", "-p", filepath.Join(tmpDir, filepath.Dir(command))).Run()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
 
-	fmt.Print(string(output))
+	err = exec.Command("cp", command, filepath.Join(tmpDir, command)).Run()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Chroot: tmpDir,
+	}
+
+	err = cmd.Run()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitError.ExitCode())
+		} else {
+			fmt.Printf("Err: %v", err)
+			os.Exit(1)
+		}
+	}
 }
